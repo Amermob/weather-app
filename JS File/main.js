b97d6c137ef80135de15eab24bd803f8faba8a00
Amermob <amermob5@gmail.com>
2025-02-04 01:22:06 +0300

    uploading the files for the weather app

---
+++ JS File/main.js
@@ -0,0 +1,21 @@
+document.querySelector(".btn").onclick = function () {
+  let cityName = document.querySelector(".input").value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=17a91727e2288c3e3ce525ccae521582&units=metric`
+  )
+    .then((respond) => respond.json())
+    .then((data) => {
+      let temp = data.main.temp.toFixed(0);
+      document.querySelector(".dig").textContent = temp + `º`;
+      document.querySelector(".cityName").textContent = data.name;
+
+      if (temp < 10) {
+        document.querySelector("img").src = "../imgs/snow.png";
+      } else if (temp < 20) {
+        document.querySelector("img").src = "../imgs/clouds.png";
+      } else if (temp > 20) {
+        document.querySelector("img").src = "../imgs/clear.png";
+      }
+    })
+    .catch(() => alert("Please Enter a Correct City Name"));
+};
